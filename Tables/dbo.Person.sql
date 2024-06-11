9860db70f3354e353497ea47f7db1dcd328b325f
dhiraj-n <dhiraj_naik@persistent.com>
2024-06-11 13:02:48 +0530

    Added new table Person.

---
+++ Tables/dbo.Person.sql
@@ -0,0 +1,8 @@
+CREATE TABLE [dbo].[Person]
+(
+[id] [int] NULL,
+[fname] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[lname] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[age] [int] NULL
+) ON [PRIMARY]
+GO
