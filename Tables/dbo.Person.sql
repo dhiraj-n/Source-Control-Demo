b3a99c3f01a8a445a0658f94c111c940f11f5d88
= <=>
2024-06-11 15:02:20 +0530

    Added Gender Column in Person Table.

--- Tables/dbo.Person.sql
+++ Tables/dbo.Person.sql
@@ -3,6 +3,7 @@ CREATE TABLE [dbo].[Person]
 [id] [int] NULL,
 [fname] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 [lname] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-[age] [int] NULL
+[age] [int] NULL,
+[gender] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
 ) ON [PRIMARY]
 GO
