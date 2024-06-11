8acbbfd9da9f36a7f20a8bba31885ec669903a51
dhiraj-n <dhiraj_naik@persistent.com>
2024-06-11 15:30:08 +0530

    Added new data.

--- Data/dbo.Person_Data.sql
+++ Data/dbo.Person_Data.sql
@@ -1,2 +1,3 @@
-INSERT INTO [dbo].[Person] ([id], [fname], [lname], [age], [gender]) VALUES (1, N'Chirag    ', N'Gaonkar   ', 22, N'Male      ')
-INSERT INTO [dbo].[Person] ([id], [fname], [lname], [age], [gender]) VALUES (2, N'Dhiraj    ', N'Naik      ', 24, N'Male      ')
+INSERT INTO [dbo].[Person] ([id], [fname], [lname], [age], [gender], [hobby]) VALUES (1, N'Chirag    ', N'Gaonkar   ', 22, N'Male      ', NULL)
+INSERT INTO [dbo].[Person] ([id], [fname], [lname], [age], [gender], [hobby]) VALUES (2, N'Dhiraj    ', N'Naik      ', 24, N'Male      ', NULL)
+INSERT INTO [dbo].[Person] ([id], [fname], [lname], [age], [gender], [hobby]) VALUES (3, N'Ritesh    ', N'Gupta     ', 25, N'Male      ', N'Coding    ')
