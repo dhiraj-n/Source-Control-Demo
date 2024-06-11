1b32cd65ef2abdcc7754262a014b91ff5d3141f6
= <=>
2024-06-11 15:14:41 +0530

    Added Static Data to Version Control

---
+++ Data/dbo.Person_Data.sql
@@ -0,0 +1,2 @@
+INSERT INTO [dbo].[Person] ([id], [fname], [lname], [age], [gender]) VALUES (1, N'Chirag    ', N'Gaonkar   ', 22, N'Male      ')
+INSERT INTO [dbo].[Person] ([id], [fname], [lname], [age], [gender]) VALUES (2, N'Dhiraj    ', N'Naik      ', 24, N'Male      ')
--- Tables/dbo.Person.sql
+++ Tables/dbo.Person.sql
@@ -1,9 +1,11 @@
 CREATE TABLE [dbo].[Person]
 (
-[id] [int] NULL,
+[id] [int] NOT NULL,
 [fname] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 [lname] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 [age] [int] NULL,
 [gender] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
 ) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[Person] ADD CONSTRAINT [PK_Person] PRIMARY KEY CLUSTERED ([id]) ON [PRIMARY]
 GO
